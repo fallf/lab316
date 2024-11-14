34c2eee93a7665804a9af3280ddc66a3031a940a
fatou fall <fatoufall95@gmail.com>
2024-11-14 15:18:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+function recursiveFunction() {
+  i++;
+  recursiveFunction();
+}
+
+try {
+  recursiveFunction();
+} catch {
+  console.log("failed");
+  console.log(i);
+}
+console.log("learning");
